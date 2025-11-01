db09533b5e871ea153e47c09bc8858711833a466
aniebietafia <aniebietafia87@gmail.com>
2025-11-01 19:54:11 +0100

    setting up project with dockerized postgres and redis
    Signed-off-by: aniebietafia <aniebietafia87@gmail.com>

---
+++ alx_backend_caching_property_listings/properties/models.py
@@ -0,0 +1,11 @@
+import uuid
+
+from django.db import models
+
+class Property(models.Model):
+    property_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    location = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
